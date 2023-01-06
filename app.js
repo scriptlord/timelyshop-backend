65d462fdd172e04701a0988737de3284d24e1edd
scriptlord <folusolibrary@gmail.com>
2023-01-07 00:49:43 +0100

    feat: add datatbase connection to app

--- app.js
+++ app.js
@@ -14,6 +14,20 @@ app.options('*', cors())
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
+
+//Database
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'eshop-database'
+})
+.then(()=>{
+    console.log('Database Connection is ready...')
+})
+.catch((err)=> {
+    console.log(err);
+})
+
 //Server
 app.listen(3000, ()=>{
 
